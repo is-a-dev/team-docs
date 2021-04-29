49240d880cf0e9bf96fe46b080d42f68cfd25e96
TheBotlyNoob <thebotlynoob@gmail.com>
2021-04-29 12:01:22 -0400

    Better!

--- md.js
+++
@@ -1,7 +0,0 @@
-export default async function getMarkdown (url) {
-  const markdown = await (await fetch (url)).text();
-  const htmlMarkdown = marked (markdown);
-  console.log(markdown);
-  console.log(htmlMarkdown);
-  document.body.innerHTML = htmlMarkdown;
-}
