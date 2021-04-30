fc39ce74fca491ca7e7e3a9f0db798d40ce6375a
TheBotlyNoob <thebotlynoob@gmail.com>
2021-04-30 12:29:49 -0400

    smaller js

--- md.js
+++ md.js
@@ -1,16 +1,11 @@
 import './marked.js';
 
 (async () => {
-  var basePath = window.location.href.substr(window.location.href.lastIndexOf('/') + 1),
-    markdown,
-    htmlMarkdown;
-  if (basePath.endsWith('.md')) { 
-    markdown = await (await fetch (`https://raw.githubusercontent.com/is-a-dev/maintainer-docs/main/${basePath}`)).text(); 
+  if (window.location.href.substr(window.location.href.lastIndexOf('/') + 1).endsWith('.md')) { 
+    document.body.innerHTML = marked (await (await fetch (`https://raw.githubusercontent.com/is-a-dev/maintainer-docs/main/${basePath}`)).text()); 
   } else { 
-    markdown = await (await fetch (`https://raw.githubusercontent.com/is-a-dev/maintainer-docs/main/${basePath}.md`)).text(); 
+    document.body.innerHTML = marked (await (await fetch (`https://raw.githubusercontent.com/is-a-dev/maintainer-docs/main/${basePath}.md`)).text()); 
   }
-  htmlMarkdown = marked (markdown);
-  document.body.innerHTML = htmlMarkdown;
 
   prism ();
 })();
