72f222b07157b14b642b6e981b7b4396a41c47f6
TheBotlyNoob <thebotlynoob@gmail.com>
2021-04-30 11:26:18 -0400

    should be a bit better

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 (async () => {
   const githubJSON = await (await fetch ('https://api.github.com/repos/is-a-dev/maintainer-docs/git/trees/main?recursive=1')).json ();
   for (i of githubJSON.tree) {
-    if (i.path.endsWith ('.md')) document.getElementById ('links').innerHTML += `<a class='link row' href='${i.path}'>${i.path}</a> \n <br> \n`;
+    if (i.path.endsWith ('.md')) document.getElementById ('links').innerHTML += `<a class='link row justify-content-center' href='${i.path}'>${i.path}</a> \n <br> \n`;
   }
 })();
