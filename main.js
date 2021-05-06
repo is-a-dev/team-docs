6bc32697e48d81af186f20449084557334c12bc3
TheBotlyNoob <thebotlynoob@gmail.com>
2021-05-06 15:54:45 -0400

    lighter colors

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
         links.push(`<a class='link row justify-content-center' href='${i.path}'>${i.path}</a> \n <br> \n`);
       }
 
-      document.getElementById ('links').innerHTML = links.join('');
+      document.getElementById ('links').innerHTML = links.join ('');
     } catch (e) {
         document.body.innerHTML = `<span class='error'>${e} <br> <br> 
         Please report this issue <a href='https://github.com/is-a-dev/maintainer-docs/issues/new?title=${e}&body=What Page Were You On When This Occured: ***${window.location.href}*** 
