fbc4df89f2b0146f064d773219a253105c9892fc
TheBotlyNoob <thebotlynoob@gmail.com>
2021-05-06 14:25:10 -0400

    better

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 (async () => {
     try {
-      document.getElementById ('links').innerHTML = '';
+      var links = [];
       for (i of (await (await fetch ('https://api.github.com/repos/is-a-dev/maintainer-docs/git/trees/main?recursive=1')).json ()).tree) {
-        document.getElementById ('links').innerHTML += `<a class='link row justify-content-center' href='${i.path}'>${i.path}</a> \n <br> \n`;
+        links.push(`<a class='link row justify-content-center' href='${i.path}'>${i.path}</a> \n <br> \n`);
       }
+
+      document.getElementById ('links').innerHTML = links.join('');
     } catch (e) {
         document.body.innerHTML = `<span class='error'>${e} <br> <br> 
         Please report this issue <a href='https://github.com/is-a-dev/maintainer-docs/issues/new?title=${e}&body=What Page Were You On When This Occured: ***${window.location.href}*** 
         <br> 
         What Was Your Error: ***${e}***'>on GitHub</a> or <a href='https://dsc.gg/is-a.dev'>on Discord</a></span>`;
+        throw e;
     }
 })();
