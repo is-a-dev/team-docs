4e5e5c47cda808db62c8a975497c2cb2946892ca
TheBotlyNoob <thebotlynoob@gmail.com>
2021-05-08 22:56:10 -0400

    maintainer area

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
-(async () => {
+'use strict';
+
+document.addEventListener('DOMContentLoaded', async () => {
     try {
       var links = [];
-      for (i of (await (await fetch ('https://api.github.com/repos/is-a-dev/maintainer-docs/git/trees/main?recursive=1')).json ()).tree) {
-        links.push(`<a class='link row container' href='${i.path}'>${i.path}</a> \n <br> \n`);
+
+      // list the documentation
+      for (const i of (await (await fetch ('https://api.github.com/repos/is-a-dev/maintainer-docs/git/trees/main?recursive=1&access_token=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')).json ()).tree) {
+        links.push (`<a class='link row container' href='${i.path}'>${i.path}</a>
+        <br>`);
       }
 
-      document.getElementById ('links').innerHTML = links.join ('');
+      document.getElementById ('links').innerHTML = links.join ('\n');
+
+      // list the maintainers on the bottom
+      for (const i of (await (await fetch ('https://api.github.com/organizations/72358814/team/4735246/members?access_token=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')).json ())) {
+        document.getElementById ('contributers').innerHTML += `
+        <div class='column maintainer'>
+          <a href='${i.html_url}'>
+            <img src='${i.avatar_url}' class='userImg' alt='${i.login}'s User Profile Image'></img>
+            <span>${i.login}</span>
+          </a>
+        </div>`;
+      }
+
     } catch (e) {
         document.body.innerHTML = `<span class='error'>${e} <br> <br> 
         Please report this issue <a href='https://github.com/is-a-dev/maintainer-docs/issues/new?title=${e}&body=What Page Were You On When This Occured: ***${window.location.href}*** 
@@ -13,4 +30,4 @@
         What Was Your Error: ***${e}***'>on GitHub</a> or <a href='https://dsc.gg/is-a.dev'>on Discord</a></span>`;
         throw e;
     }
-})();
+});
