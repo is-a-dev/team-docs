9c5f7c1b20973b9b21d2e19f7fc92503bad99af2
Jay Jackson <thebotlynoob@gmail.com>
2022-03-08 14:58:13 -0500

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', async () => {
       for (const i of (await (await fetch ('https://api.github.com/orgs/is-a-dev/members', {
         method: 'GET',
         headers: {
-          'Authorization': `token ${await fetch ('https://raw.githubusercontent.com/is-a-dev/maintainer-docs/gh-pages/token.txt').text()}`
+          'Authorization': `token ${await fetch ('https://raw.githubusercontent.com/is-a-dev/maintainer-docs/gh-pages/token.txt').text().replace('-', '')}`
         }
       })).json ())) {
         document.getElementById ('contributers').innerHTML += `
