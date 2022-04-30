a769e1594a13e5353e6ab977d7ffce4691623bb8
Jay Jackson <jj@jj.is-a.dev>
2022-04-30 14:24:23 -0400

    Revert "feat: use env vars instead of public tokens"

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', async () => {
       for (const i of (await (await fetch ('https://api.github.com/orgs/is-a-dev/members', {
         method: 'GET',
         headers: {
-          'Authorization': `token ${process.env.GH_TOKEN}`
+          'Authorization': `token ${(await fetch('https://raw.githubusercontent.com/is-a-dev/maintainer-docs/gh-pages/token.txt').then(r => r.text())).replace(/\-/g, '')}`
         }
       })).json ())) {
         document.getElementById ('contributers').innerHTML += `
