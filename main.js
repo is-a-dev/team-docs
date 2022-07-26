87c8e2b5131799b6bc3e51d386558b8637dd120b
Mahir Molai <mahir@molai.dev>
2022-07-27 00:13:59 +0400

    update

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", async () => {
-    const headers = {
+    /**
+     * const headers = {
         Authorization: `token ${(
             await fetch(
                 "https://raw.githubusercontent.com/is-a-dev/maintainer-docs/gh-pages/token.txt"
             ).then((r) => r.text())
         ).replace(/\-/g, "")}`,
     };
+     */
 
     let links = [];
 
     // list the documentation
     for (const i of (
         await fetch(
-            "https://api.github.com/repos/is-a-dev/maintainer-docs/git/trees/main?recursive=1",
-            { headers }
+            "https://api.github.com/repos/is-a-dev/maintainer-docs/git/trees/main?recursive=1" // ,{ headers }
+            
         ).then((res) => res.json())
     ).tree) {
         links.push(
-            `<a class='link row container' href='${i.path}'>${i.path}</a><br>`
+            `<a class='link' href='${i.path}'>${i.path}</a><br>`
         );
     }
 
@@ -27,12 +29,15 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     // list the maintainers on the bottom
     for (const i of await (
-        await fetch("https://api.github.com/orgs/is-a-dev/members", {
+        await fetch("https://api.github.com/orgs/is-a-dev/members")
+        /**
+         * , {
             headers,
-        })
+        }
+         */
     ).json()) {
-        document.getElementById("contributers").innerHTML += `
-        <div class='column maintainer'>
+        document.getElementById("maintainers").innerHTML += `
+        <div class='maintainer'>
           <a href='${i.html_url}'>
             <img src='${i.avatar_url}' alt='${i.login}'s User Profile Image'></img>
             <span>${i.login}</span>
